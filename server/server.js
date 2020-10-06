61b462dfb732811c3dda4b728984a99269b1f257
Marshall Augey <marshall@aremde.com.au>
2020-10-06 18:14:17 +1000

    image upload scaf working/ updated seeders

--- server/server.js
+++ server/server.js
@@ -10,20 +10,23 @@ const server = require('./listen.js');
 const builder = require('./mongodb/BuildScripts');
 const deleter = require('./mongodb/removescripts');
 const fetcher = require('./mongodb/viewscripts');
-
+const bodyParser = require('body-parser')
 const PORT = 3000;
-
 app.use(cors());
-
+app.use(bodyParser.json());
 //image upload part
-app.use(express.static(path.join(__dirname , '../dist/imageupload/')));
-app.use('/images', express.static(path.join(__dirname, './images')));
+app.use(express.static(path.join(__dirname , '../chat/dist/chat')));
+
+app.use('/images', express.static(path.join(__dirname, './userimages')));
 require('./routes/uploads.js')(app, formidable);
 
 sockets.connect(io, PORT);
 
 server.listen(http, PORT);
 // load mongo into jsons 
+app.get('/',function(req,res){
+    res.sendFile(path.join(__dirname , '../chat/dist/chat/index.html'));
+});
 
 app.get('/build',function(req,res){
     builder.build();
